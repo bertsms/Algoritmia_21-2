8c6a03fcee06181e8844348e759a9cb15a8998eb
bertsms <83386312+bertsms@users.noreply.github.com>
2021-04-28 23:39:21 -0500

    Add files via upload

---
+++ Entregable_S5.py
@@ -0,0 +1,10 @@
+def pedirDatos(nombre, ApellidoPaterno, ApellidoMaterno, clave, salon):
+    Datos = {Ana, Lopez, Hernandez, 170654, 1004}
+    print() # Nombre Completo: Ana Lopez Hernandez
+    print() # Clave: 170654
+    return Datos
+
+datos1 = pedirDatos("Ana", 'Lopez', "Hernandez", 170654, 1004)
+
+# {nombre: "Ana", apellidoPaterno: Lopez, clave: 1170654
+}
