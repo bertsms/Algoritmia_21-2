cc00596d68f0c0e0972c6c9d910c5163b3a6d378
bertsms <83386312+bertsms@users.noreply.github.com>
2021-04-28 23:21:02 -0500

    Add files via upload

---
+++ S2.py
@@ -0,0 +1,20 @@
+#Esto es un comentario
+# Input siempre regresa una string(palabra)
+
+
+# Nombre
+nombre = "Bertha"
+print("Mi nombre es " + nombre)
+
+# Apellidos
+apellidoPaterno = "Mendoza"
+apellidoMaterno = "Sanchez"
+apellidos = apellidoPaterno + ' ' + apellidoMaterno
+print("Mis Apellidos son: " + apellidos)
+
+# Edad
+edad = 0
+print("Edad:", edad)
+edad = int(input("Cual es tu edad? "))  # "26" -> 21
+print("Edad:", edad)
+print("Edad + 5:", edad + 5)
