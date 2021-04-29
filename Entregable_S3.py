0800837e32c2d112515ecf7457f2f302f7138b26
bertsms <83386312+bertsms@users.noreply.github.com>
2021-04-28 23:35:02 -0500

    Add files via upload

---
+++ Entregable_S3.py
@@ -0,0 +1,20 @@
+animal = {
+    'nombre': "leon",
+    'colorPrimario': "mostaza",
+    'colorSecundario':"cafe",
+    'peso':, "200"
+    'amistoso':, FALSE
+    'añosDeVida':, "3"
+    '',
+    '',
+    '',
+}
+print(animal['nombre'])
+print(animal['peso'])
+print(type('peso'))
+print(animal['colorPrimario'])
+print(animal['colorSecundario']) 
+print(animal['amistoso'])
+print(animal['añosDeVida'])
+print(animal['sobrenombre'])
+
